c39a567fa897d7e3025ea505a395b0c26a0c0246
SenseiDeath <alecparent@rocketmail.com>
2022-12-08 21:47:36 -0700

    removed path routing from github

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  // });
 }
 
 //Routes
