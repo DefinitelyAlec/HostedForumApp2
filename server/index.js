ada3d250cae0c46ce58f87931b9df7171e07b7ff
SenseiDeath <alecparent@rocketmail.com>
2022-12-08 22:31:48 -0700

    route to index after gets and before posts

--- server/index.js
+++ server/index.js
@@ -8,12 +8,7 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-// PATH CONFIGURATION TO RESPOND TO A REQUEST TO STATIC ROUTE REQUEST BY SERVING index.html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
 
 // if (process.env.NODE_ENV === 'production') {
 //   // Exprees will serve up production assets
@@ -85,20 +80,6 @@ app.get("/checkUserName/:user", async (req, res) => {
   }
 });
 
-//Add a new user to the database
-app.post("/newUser", async (req, res) => {
-  try {
-    const { email, name, imgurl } = req.body;
-    const newUser = await pool.query(
-      "INSERT INTO users (username, email, imgurl) VALUES($1, $2, $3) RETURNING *",
-      [name, email, imgurl]
-    );
-    res.json(newUser.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
 // get a users previous tips
 app.get("/tips/:user_id", async (req, res) => {
   try {
@@ -136,6 +117,111 @@ app.delete("/tips/tip/:tip_id", async (req, res) => {
       [tip_id]
     );
     res.json("Tip deleted.")
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//get all threads
+app.get("/threads", async (req, res) => {
+  try {
+    const allThreads = await pool.query("SELECT * FROM thread WHERE tipid IS NULL");
+    res.json(allThreads.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//get all tip threads
+app.get("/tipthreads", async (req, res) => {
+  try {
+    const allThreads = await pool.query("SELECT * FROM thread WHERE tipid IS NOT NULL");
+    res.json(allThreads.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//queries database for the thread contents of a given thread. returns all rows.
+app.get("/threads/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const oneThread = await pool.query(
+      "SELECT thread_contents.content_id, thread_contents.user_id, users.username, thread_contents.contents, \
+       users.imgurl, thread_contents.unix_time, thread_contents.like_dislike_ratio \
+       FROM thread_contents LEFT JOIN users ON thread_contents.user_id = users.user_id \
+       WHERE thread_contents.thread_id = $1 \
+       ORDER BY thread_contents.unix_time ASC",
+      [id]
+    );
+    res.json(oneThread.rows);
+    // oneThread.rows.map((thread) => {
+    //   console.log(thread);
+    // });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// PATH CONFIGURATION TO RESPOND TO A REQUEST TO STATIC ROUTE REQUEST BY SERVING index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// posts a comment to a thread
+app.post("/threads/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {user_id, contents} = req.body;
+    const curr_time = Date.now();
+    const insertComment = await pool.query("INSERT INTO thread_contents(thread_id, user_id, contents, unix_time) \
+     VALUES($1, $2, $3, $4) RETURNING *",
+    [id, user_id, contents, curr_time]);
+    res.json(insertComment.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//update thread
+app.put("/threads/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateThread = await pool.query(
+      "UPDATE thread SET description = $1 WHERE thread_id = $2",
+      [description, id]
+    );
+    res.json("Thread is updated");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//Update like/dislike ratio on a comment
+app.put("/threads/:thread_id/:content_id/changeLikeRatio", async (req, res) => {
+  try {
+    const {thread_id, content_id} = req.params;
+    const {amountToChange} = req.body;
+    const likeComment = await pool.query(
+      "UPDATE thread_contents SET like_dislike_ratio = like_dislike_ratio + $1 WHERE content_id = $2",
+      [amountToChange, content_id]
+    );
+    res.json("Comment liked!");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//Add a new user to the database
+app.post("/newUser", async (req, res) => {
+  try {
+    const { email, name, imgurl } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users (username, email, imgurl) VALUES($1, $2, $3) RETURNING *",
+      [name, email, imgurl]
+    );
+    res.json(newUser.rows);
   } catch (error) {
     console.error(error.message);
   }
@@ -192,92 +278,6 @@ app.post("/threads", async (req, res) => {
     );
 
     res.json(newThread.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-//get all threads
-app.get("/threads", async (req, res) => {
-  try {
-    const allThreads = await pool.query("SELECT * FROM thread WHERE tipid IS NULL");
-    res.json(allThreads.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-//get all tip threads
-app.get("/tipthreads", async (req, res) => {
-  try {
-    const allThreads = await pool.query("SELECT * FROM thread WHERE tipid IS NOT NULL");
-    res.json(allThreads.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-//queries database for the thread contents of a given thread. returns all rows.
-app.get("/threads/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const oneThread = await pool.query(
-      "SELECT thread_contents.content_id, thread_contents.user_id, users.username, thread_contents.contents, \
-       users.imgurl, thread_contents.unix_time, thread_contents.like_dislike_ratio \
-       FROM thread_contents LEFT JOIN users ON thread_contents.user_id = users.user_id \
-       WHERE thread_contents.thread_id = $1 \
-       ORDER BY thread_contents.unix_time ASC",
-      [id]
-    );
-    res.json(oneThread.rows);
-    // oneThread.rows.map((thread) => {
-    //   console.log(thread);
-    // });
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// posts a comment to a thread
-app.post("/threads/:id", async (req, res) => {
-  try {
-    const {id} = req.params;
-    const {user_id, contents} = req.body;
-    const curr_time = Date.now();
-    const insertComment = await pool.query("INSERT INTO thread_contents(thread_id, user_id, contents, unix_time) \
-     VALUES($1, $2, $3, $4) RETURNING *",
-    [id, user_id, contents, curr_time]);
-    res.json(insertComment.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-//update thread
-app.put("/threads/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateThread = await pool.query(
-      "UPDATE thread SET description = $1 WHERE thread_id = $2",
-      [description, id]
-    );
-    res.json("Thread is updated");
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-//Update like/dislike ratio on a comment
-app.put("/threads/:thread_id/:content_id/changeLikeRatio", async (req, res) => {
-  try {
-    const {thread_id, content_id} = req.params;
-    const {amountToChange} = req.body;
-    const likeComment = await pool.query(
-      "UPDATE thread_contents SET like_dislike_ratio = like_dislike_ratio + $1 WHERE content_id = $2",
-      [amountToChange, content_id]
-    );
-    res.json("Comment liked!");
   } catch (error) {
     console.error(error.message);
   }
